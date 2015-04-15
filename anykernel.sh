39a7042b964d417e6fe4e5e1b6ce4205bb6bbc4f
DerRomtester <dieb.stefan.96@gmail.com>
2015-04-15 17:05:41 +0200

    anykernel build script
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Cleaning old files"
+rm -f ../opo-AnyKernel2/dtb
+rm -f ../opo-AnyKernel2/zImage
+rm -f ../output/Tyr*.zip
+echo "Making oneplus one kernel"
+DATE_START=$(date +"%s")
+
+make clean && make mrproper
+
+export ARCH=arm
+export SUBARCH=arm
+make Tyr_defconfig
+make -j8
+echo "End of compiling kernel!"
+
+DATE_END=$(date +"%s")
+echo
+DIFF=$(($DATE_END - $DATE_START))
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+
+if [ $# -gt 0 ]; then
+echo $1 > .version
+fi
+../ramdisk_one_plus_one/dtbToolCM -2 -o ../opo-AnyKernel2/dtb -s 2048 -p ../one_plus_one/scripts/dtc/ ../one_plus_one/arch/arm/boot/
+cp arch/arm/boot/zImage ../opo-AnyKernel2/zImage
+cd ../opo-AnyKernel2/
+zipfile="TyrV.zip"
+zip -r -9 $zipfile *
+mv TyrV*.zip ../output/
