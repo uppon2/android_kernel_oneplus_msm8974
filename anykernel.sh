b9b4cae48424192310c2abe83bd122502af88fc1
DerRomtester <dieb.stefan.96@gmail.com>
2015-04-16 18:48:58 +0200

    Version 106
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -8,6 +8,8 @@ DATE_START=$(date +"%s")
 
 make clean && make mrproper
 
+VER=106
+export KBUILD_BUILD_VERSION=$VER
 export ARCH=arm
 export SUBARCH=arm
 make Tyr_defconfig
@@ -19,12 +21,9 @@ echo
 DIFF=$(($DATE_END - $DATE_START))
 echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 
-if [ $# -gt 0 ]; then
-echo $1 > .version
-fi
 ../ramdisk_one_plus_one/dtbToolCM -2 -o ../opo-AnyKernel2/dtb -s 2048 -p ../one_plus_one/scripts/dtc/ ../one_plus_one/arch/arm/boot/
 cp arch/arm/boot/zImage ../opo-AnyKernel2/zImage
 cd ../opo-AnyKernel2/
-zipfile="TyrV.zip"
+zipfile="TyrV-$VER.zip"
 zip -r -9 $zipfile *
 mv TyrV*.zip ../output/
